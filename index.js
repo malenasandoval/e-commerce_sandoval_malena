a40865218375d35124a0cf46e18d2b6a0309cca4
nombre <malenasando@gmail.com>
2024-09-02 20:54:25 -0300

    modificacion de cards con document.querySelector

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("HOLAAAAAA")
+
+const title1 = "SOMBRA PARA OJOS";
+const title2 = "GLOSS PARA LABIOS";
+const title3 = "SOMBRA PARA OJOS";
+
+document.querySelector("#title1").innerText = title1;
+document.querySelector("#title2").innerText = title2;
+document.querySelector("#title3").innerText = title3;
+
+const precio1 = "$8.300";
+const precio2 = "$6.900";
+const precio3 = "$13.900";
+document.querySelector("#precio1").innerText = precio1;
+document.querySelector("#precio2").innerText = precio2;
+document.querySelector("#precio3").innerText = precio3;
+
