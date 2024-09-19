ddfae9d2b99df4a7e35b54f0d2a89b2af60c7223
nombre <malenasando@gmail.com>
2024-09-19 01:12:24 -0300

    envolver codigo js en una funcion

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-document.querySelector('h1').textContent = 'Productos';
+document.querySelector('h1').innerText = 'Productos';
 
-const array = [];
+function productos() {
 
-const container = document.querySelector('section.container-cards');
+    const array = [];
 
-for (let i = 1; i <= 4; i++) {
-    const card = `
+    for (let i = 1; i <= 4; i++) {
+        const card = `
         <div class="card">
           <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="Producto ${i}" />
           <div class="card-body">
             <h2 id="title${i}" class="card-title">Producto ${i}</h2>
             <h2 id="precio${i}">$precio</h2>
-            <a href="#" class="btn btn-primary">Comprar</a>
+            <a href="#" class="btn btn-primary">Ver más</a>
           </div>  
         </div>
       `;
-    array.push(card);
+        array.push(card);
+    }
+
+    document.querySelector("section").innerHTML = array.join('');
 }
 
-container.innerHTML = array.join('');
-
+productos()
